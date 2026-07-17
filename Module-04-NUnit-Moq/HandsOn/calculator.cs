3a92c867167ac1050bdf3e74c0c5c9cdc60524e9
Ritesh kumar <riteshsingh1628@gmail.com>
2026-07-17 20:34:40 +0530

    Add Calculator class with Add and Subtract methods

---
+++ Module-04-NUnit-Moq/HandsOn/calculator.cs
@@ -0,0 +1,14 @@
+namespace NUnitHandsOn
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
