6bbf0256a4c4b8d890805eddb3e4de6015837c13
Ritesh kumar <riteshsingh1628@gmail.com>
2026-07-17 22:13:13 +0530

    Add Product model for retail inventory system

---
+++ Module-05-EF-Core/HandsOn/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace RetailInventorySystem.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
