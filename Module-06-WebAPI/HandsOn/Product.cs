c922afb586d7078232ae518c423b96ccc7e8c8c5
Ritesh kumar <riteshsingh1628@gmail.com>
2026-07-20 08:57:01 +0530

    Create Product.cs

---
+++ Module-06-WebAPI/HandsOn/Product.cs
@@ -0,0 +1,10 @@
+namespace RetailInventoryAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
